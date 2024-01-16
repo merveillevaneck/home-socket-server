9fc4ecb70d83ea445bf11a8942a3fadbf8490226
Merveille van Eck <merveillevaneck@gmail.com>
2024-01-16 14:26:57 +0200

    created a basic websocket in main without CORS

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use axum::routing::get;
+use socketioxide::{extract::SocketRef, SocketIo};
+
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+
+fn on_connect(socket: SocketRef) {
+    info!("Socket:io connected: {:?} {:?}", socket.ns(), socket.id);
+    println!("connected");
+
+    socket.emit("message", "hello");
+}
+
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder().finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/", on_connect);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(layer);
+
+    info!("starting server on port 3001");
+    axum::Server::bind(&"127.0.0.1:3001".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
